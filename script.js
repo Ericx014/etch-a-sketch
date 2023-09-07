2a55817157e116de21e446e67a85d1bc35f6fad1
Eric Bong <ericbong6868@gmail.com>
2023-09-07 14:12:09 +0800

    Added basic funcionalities

--- script.js
+++ script.js
@@ -1,13 +1,83 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('.grid-container');
+const DEFAULT_SIZE = 16;
+let mode = '';
 
-function createGrid(rows, cols) {
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-            const square = document.createElement('div');
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+// Function to clearGrid
+function clearGrid(){
+    container.innerHTML = "";
+}
+
+// Function to create grid
+function createGrid(size) {
+
+    // Remove existing grid before generating new grid
+    clearGrid();
+
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+
+    for (let i=0; i< size*size; i++){
+        const square = document.createElement('div');
             square.classList.add('square');
+            container.addEventListener('mouseover', changeColor);
+            container.addEventListener('mousedown', changeColor);
             container.appendChild(square);
-        }
     }
 }
 
-createGrid(16, 16);
+// Action listener for button
+// Button to adjust grid size
+const adjustGridButton = document.querySelector('.adjust-grid-button');
+adjustGridButton.addEventListener('click', () => {
+    
+    const squarePerSide = prompt("Enter number of squares per side: ");
+
+    if ((squarePerSide > 0) && (squarePerSide <= 100)){
+        createGrid(squarePerSide);
+        container.style.gridTemplateColumns = `repeat(${squarePerSide}, 1fr)`;
+    } else {
+        alert('Please enter a number from 1 to 100 only.');
+    }
+   
+})
+
+function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseDown) return;
+    
+    if (e.type === 'mouseover' && mouseDown) {
+        e.target.style.backgroundColor = 'black';
+    }
+}
+
+const clearButton = document.querySelector(".clear-button");
+clearButton.addEventListener('click', () => {
+    const allSquare = document.querySelectorAll(".square");
+    allSquare.forEach((square) => {
+        square.style.backgroundColor = "white";
+    });
+});
+
+// const randomColorButton = document.querySelector(".random-color-button");
+// randomColorButton.addEventListener('Click', () => {
+
+// })
+
+// function modeSelector() {
+//     randomColorButton.addEventListener('Click', () => {
+//         mode = 'randomColor';
+//     })
+// }
+
+// function changeColor(){
+    
+// }
+
+window.onload = () => {
+    createGrid(DEFAULT_SIZE);
+}
+
